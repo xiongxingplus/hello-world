0802b5d9bc033e935bfc1dfad916f7f0075637b5
xiong xing <ctgu.edu.cn@gmail.com>
2016-10-23 20:29:37 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by user on 2016/10/23.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello world");
+    }
+}
